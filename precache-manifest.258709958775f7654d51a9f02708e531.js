2c5ee7a413e690226d414d905ebc1e1189954cd1
Parker Allen <parkerjallen1@gmail.com>
2021-07-07 00:07:37 -0700

    Updates

---
+++ precache-manifest.258709958775f7654d51a9f02708e531.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f857cd510664ab574c2bd485973d9e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8aec5feb646b9596b8c1",
+    "url": "./static/css/2.72b1f475.chunk.css"
+  },
+  {
+    "revision": "c872cbb752fa513c742f",
+    "url": "./static/css/main.e7caba5e.chunk.css"
+  },
+  {
+    "revision": "8aec5feb646b9596b8c1",
+    "url": "./static/js/2.5daa71fb.chunk.js"
+  },
+  {
+    "revision": "ca228484939fe4d1fc04f68ed86646c0",
+    "url": "./static/js/2.5daa71fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c872cbb752fa513c742f",
+    "url": "./static/js/main.789b7134.chunk.js"
+  },
+  {
+    "revision": "da503e7975902b161ea8",
+    "url": "./static/js/runtime-main.7d63d1b5.js"
+  },
+  {
+    "revision": "a4e80c1e977416ec45d808a3268892b7",
+    "url": "./static/media/builton.a4e80c1e.svg"
+  },
+  {
+    "revision": "2faeb737982f9eb144d67f15a9e00226",
+    "url": "./static/media/plus-jakarta-sans-latin-400-normal.2faeb737.woff2"
+  },
+  {
+    "revision": "805de6e3dec77f8f00604d73f3d04274",
+    "url": "./static/media/plus-jakarta-sans-latin-400-normal.805de6e3.woff"
+  }
+]);
